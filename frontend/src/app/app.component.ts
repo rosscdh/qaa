d9edb88fc76ca269f35c8d4878a959ead77ab3e6
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2018-07-15 14:44:29 +0200

    stuff

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -11,7 +11,7 @@ import { Component } from '@angular/core';
     </nav>
     <router-outlet></router-outlet>
   `,
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', 'bulma.min.css']
 })
 export class AppComponent {
   title = 'Questions & Answers';
